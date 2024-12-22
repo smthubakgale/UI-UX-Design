6c39e0ee9aea9b1f6c1a88a5f8afacc998ac20db
smthubakgale <97933623+smthubakgale@users.noreply.github.com>
2024-12-23 00:23:02 +0200

    Update script.js

--- frameworks/ux2/script.js
+++ frameworks/ux2/script.js
@@ -4,7 +4,22 @@ document.querySelector('.toggle-sidebar').addEventListener('click', function() {
 
 // Optional: Add this to make the sidebar toggle on smaller screens
 window.addEventListener('resize', function() {
-    if (window.innerWidth > 768) {
+    if (window.innerWidth <= 768) {
+        document.body.classList.add('sidebar-toggled');
+    } else {
         document.body.classList.remove('sidebar-toggled');
+    }
+});
+
+// Toggle top menu on smaller screens
+window.addEventListener('resize', function() {
+    if (window.innerWidth <= 480) {
+        document.querySelectorAll('.top-menu li').forEach((li) => {
+            li.querySelector('ul').style.display = 'none';
+        });
+    } else {
+        document.querySelectorAll('.top-menu li').forEach((li) => {
+            li.querySelector('ul').style.display = 'block';
+        });
     }
 });
